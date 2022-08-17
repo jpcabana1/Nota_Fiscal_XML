6d5fc1644dff4f0f6678149e1e81b94bc4e4cc3c
João Pedro <jpcabana1@gmail.com>
2022-08-16 21:45:57 -0300

    Criação da view VIEW_RELATORIO_NOTA

--- scripts-migration/nfe-stage.sql
+++ scripts-migration/nfe-stage.sql
@@ -85,3 +85,19 @@ ADD dtProducao DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP;
 
 ALTER TABLE NOTA_FISCAL.importacao_nota_xml
 alter COLUMN dtProducao DATETIME;
+
+CREATE VIEW NOTA_FISCAL.VIEW_RELATORIO_NOTA
+AS 
+    SELECT 
+        INX.idNota AS "Id"
+        ,INX.xChave AS "Chave de Acesso"
+        ,PN.xProd AS "Descrição"
+        ,PN.uCom AS "Unidade"
+        ,PN.qCom AS "Quantidade"
+        ,PN.vProd AS "Valor"
+        ,FORMAT(CAST(INX.dtProducao AS DATE), 'dd/MM/yyyy') AS  "Data de Emissão"
+    FROM NOTA_FISCAL.importacao_nota_xml AS INX INNER JOIN NOTA_FISCAL.produto_nota AS PN
+        ON INX.xChave = PN.xChave
+        AND INX.xHash = PN.xHash
+        AND INX.idNota = PN.idNota
+GO
