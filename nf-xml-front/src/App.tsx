4d6583ad7870bf57c0fe21f9d595d44e87389f96
João Pedro <jpcabana1@gmail.com>
2022-12-10 13:26:29 -0300

    Changed .net to 6.0

--- nf-xml-front/src/App.tsx
+++ nf-xml-front/src/App.tsx
@@ -7,7 +7,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        
         <p><b>Ambiente:</b> {process.env.REACT_APP_AMBIENTE}</p>
         <p><b>Url importação:</b> {process.env.REACT_APP_API_IMPORTACAO_URL}</p>
       </header>
